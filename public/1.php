133fff6a02b339fb5f91501e754b1a12f25f385b
ildragone888888 <48631634+ildragone888888@users.noreply.github.com>
2023-02-13 21:56:43 +0300

    Update 1.php

--- public/1.php
+++ public/1.php
@@ -1,31 +1,82 @@
 <?php
+$__content__ = '';
 
-        // create curl resource
+function echo_content($content) {
+global $__password__;
+list($nameff, $namefr) = namef();
 
-        $ch = curl_init();
+echo $content;
+}
+function curl_header_function($ch, $header) {
+global $__content__;
+$pos = strpos($header, ':');
+if ($pos == false) {
+$__content__ .= $header;
+} 
+else {
+$key = join('-', array_map('ucfirst', explode('-', substr($header, 0, $pos))));
+if ($key != 'Transfer-Encoding') {
+$__content__ .= $key . substr($header, $pos);
+}
+}
+return strlen($header);
+}
+function curl_write_function($ch, $content) {
+global $__content__;
+if ($__content__) {
+echo_content($__content__);
+$__content__ = '';
+}
+echo_content($content);
+return strlen($content);
+}
+ 
+function post($method, $url, $body) {
 
-
-
-        // set url
-
-        curl_setopt($ch, CURLOPT_URL, "google.com");
-
-
-
-        //return the transfer as a string
-
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-
-
-
-        // $output contains the output string
-
-       echo $output = curl_exec($ch);
-
-
-
-        // close curl resource to free up system resources
-
-        curl_close($ch);      
-
-?>
+$curl_opt = array();
+$ch = curl_init();
+$curl_opt[CURLOPT_URL] = $url;
+switch (strtoupper($method)) {  
+case 'GET':
+break;
+case 'HEAD':
+$curl_opt[CURLOPT_NOBODY] = true;
+break;
+case 'OPTIONS':
+case 'TRACE':
+$curl_opt[CURLOPT_CUSTOMREQUEST] = $method;
+break;
+case 'POST':
+case 'PATCH':
+case 'PUT':
+case 'DELETE':
+$curl_opt[CURLOPT_CUSTOMREQUEST] = $method;
+if ($body) {
+$curl_opt[CURLOPT_POSTFIELDS] = $body;
+}
+break;
+default:
+echo_content("HTTP/1.0 502\r\n\r\n" . message_html('502 Urlfetch Error', 'Method error ' . $method));
+exit(-1);
+}
+$curl_opt[CURLOPT_HTTPHEADER] = $header_array;
+$curl_opt[CURLOPT_RETURNTRANSFER] = true;
+$curl_opt[CURLOPT_CONNECTTIMEOUT] = 10;
+$curl_opt[CURLOPT_TIMEOUT] = 19;
+$curl_opt[CURLOPT_HEADERFUNCTION] = 'curl_header_function';
+$curl_opt[CURLOPT_WRITEFUNCTION]  = 'curl_write_function';
+$curl_opt[CURLOPT_SSL_VERIFYPEER] = false;
+$curl_opt[CURLOPT_SSL_VERIFYHOST] = false;
+curl_setopt_array($ch, $curl_opt);
+curl_exec($ch);
+if (curl_errno($ch)) {
+$error0 = curl_error($ch);
+echo_content("HTTP/1.0 502\r\n\r\n" . message_html('502 Urlfetch Error', 'URL error ' . $error0));
+}
+curl_close($ch);
+if ($GLOBALS['__content__']) {
+echo_content($GLOBALS['__content__']);
+} 
+}
+$body = '';
+post("GET", "https://google.com/", $body);

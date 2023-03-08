1715aa548107f5bd1c55b78185a7d4c9f25ea7fd
ildragone888888 <48631634+ildragone888888@users.noreply.github.com>
2023-03-08 15:02:49 +0300

    Update index.php

--- index.php
+++ index.php
@@ -67,8 +67,7 @@ global $__password__;
 list($nameff, $namefr) = namef();
 //header('Content-type: '.$namefr.'');
 //header('Content-Disposition: attachment; filename='.$nameff.'');
-//echo $content ^ str_repeat($__password__[0], strlen($content));
-  echo $content;
+echo $content ^ str_repeat($__password__[0], strlen($content));
 }
 function curl_header_function($ch, $header) {
 global $__content__;
